30ad2767c5cf03592ee42ead268406f44a41930f
Juan Pablo <juan_pablo_rojas@hotmail.com>
2024-07-13 21:50:52 -0300

    add

--- RenderApiDeploy/RenderApiDeploy/Controllers/WeatherForecastController.cs
+++ RenderApiDeploy/RenderApiDeploy/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ namespace RenderApiDeploy.Controllers
         [HttpGet("RenderDeploy")]
         public string RenderDeploy()
         {
-            return "Render Deploy";
+            return "Render Deploy api";
         }
     }
 }
